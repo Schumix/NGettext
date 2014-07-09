e0377b0024d3a4e666cdb4a42379b479313754d9
Megax <megax@yeahunter.hu>
2014-07-09 19:00:38 +0200

    * Scriptek és travis hozzáadva. Nunit-framework hozáadva.

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+xbuild /p:Configuration="Debug" src/NGettext.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
+
+cd src/NGettext.Tests/bin/Debug
+nunit-console Tests.dll
